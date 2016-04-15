ecd6f8033ed3c7a7a4963c4407338e2a369d67eb
whx <907356000@qq.com>
2016-04-15 17:08:54 +0800

    更改点赞策略，更新了爬虫爬取策略

--- pachong/demo.py
+++ pachong/demo.py
@@ -22,7 +22,7 @@ def getHtml(url):
 html = getHtml("https://linux.cn/news")
 #print html
 soup = BeautifulSoup(str(html),'html.parser',from_encoding='utf-8')
-print soup
+#print soup
 From = soup.title.get_text()
 Div = soup.find_all('div',class_='carousel-caption')
 #清空原来数据
@@ -36,12 +36,17 @@ for line in Div:
     html2 = getHtml(soup2.find('a')['href'])
     Tltle = BeautifulSoup(html2,'html.parser',from_encoding='utf-8').find('h1',class_='ph').get_text()
     Img = soup.find('img',alt='%s' % Tltle.strip())['src']
-    print Img
-    sql="INSERT INTO `dj_bbs_news`(title,summary,url,favor_count,reply_count,create_date,image_urls) VALUES('%s','%s','%s','0','0',NOW(),'news/%s.jpg');"%(Tltle,From,Url,x)
-    Mysql.MysqlHelper().In_sql(sql)
-    local = os.path.join('d:/','%s.jpg' % x)
+    #print Img
+    try:
+        sql='''INSERT INTO `dj_bbs_news`(title,summary,url,favor_count,reply_count,create_date,image_urls) VALUES("%s",'%s','%s','0','0',NOW(),'news/%s.jpg');''' %(Tltle,From,Url,x)
+        #print sql
+        Mysql.MysqlHelper().In_sql(sql)
+    except Exception,e:
+        print e
+    local = os.path.join('D:/test/bbs/static/news/','%s.jpg' % x)
     urllib.urlretrieve(Img,local)
     #处理图片
-    Picture.timage("d:/%s.jpg" % x, 'd:/')
+    Picture.timage("D:/test/bbs/static/news/%s.jpg" % x, 'D:/test/bbs/static/news/')
+    print '已处理完%s.jpg'% x
     x+=1
 
